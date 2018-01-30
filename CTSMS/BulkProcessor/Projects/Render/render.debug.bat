1d4bc5f09f82fa148daf3923aa31b11d219081c6
Rene Krenn <rkrenn@phoenixctms.org>
2018-01-30 01:32:36 +0100

    render.debug.bat

---
+++ CTSMS/BulkProcessor/Projects/Render/render.debug.bat
@@ -0,0 +1,15 @@
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=cleanup_all --force
+
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_ecrfstatustype_diagram
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_courseparticipationstatustype_diagrams
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_privacyconsentstatustype_diagram
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_trialstatustype_diagram
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_probandliststatustype_diagram
+perl process.pl --config=config.debug.cfg --settings=settings.debug.cfg --task=create_ecrffieldstatustype_diagrams
+
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_ecrfstatustype_diagram
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_courseparticipationstatustype_diagrams
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_privacyconsentstatustype_diagram
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_trialstatustype_diagram
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_probandliststatustype_diagram
+perl process.pl --config=config_de.debug.cfg --settings=settings_de.debug.cfg --task=create_ecrffieldstatustype_diagrams
