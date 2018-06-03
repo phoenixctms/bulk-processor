e77616dea538167be59c64b89b7621b7d5e77d61
Rene Krenn <rkrenn@phoenixctms.org>
2018-06-03 19:16:40 +0200

    normalize line endings

--- CTSMS/BulkProcessor/Projects/Render/render.sh
+++ CTSMS/BulkProcessor/Projects/Render/render.sh
@@ -1,6 +1,5 @@
 perl process.pl --config=config.cfg --settings=settings.cfg --task=cleanup_all --force
 
-perl process.pl --config=config.cfg --settings=settings.cfg --task=create_ecrfstatustype_diagram
 perl process.pl --config=config.cfg --settings=settings.cfg --task=create_courseparticipationstatustype_diagrams
 perl process.pl --config=config.cfg --settings=settings.cfg --task=create_privacyconsentstatustype_diagram
 perl process.pl --config=config.cfg --settings=settings.cfg --task=create_trialstatustype_diagram
