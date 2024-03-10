3a08e8b1f89fcf959851cd882c03f9e151721cf1
Rene Krenn <rkrenn@phoenixctms.org>
2024-03-10 13:08:13 +0100

    render state diagrams in spanish language

--- CTSMS/BulkProcessor/Projects/Render/render.sh
+++ CTSMS/BulkProcessor/Projects/Render/render.sh
@@ -15,3 +15,11 @@ perl process.pl --config=config_de.cfg --settings=settings_de.cfg --task=create_
 perl process.pl --config=config_de.cfg --settings=settings_de.cfg --task=create_probandliststatustype_diagram
 perl process.pl --config=config_de.cfg --settings=settings_de.cfg --task=create_ecrffieldstatustype_diagrams
 perl process.pl --config=config_de.cfg --settings=settings_de.cfg --task=create_massmailstatustype_diagram
+
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_ecrfstatustype_diagram
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_courseparticipationstatustype_diagrams
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_privacyconsentstatustype_diagram
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_trialstatustype_diagram
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_probandliststatustype_diagram
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_ecrffieldstatustype_diagrams
+perl process.pl --config=config_es.cfg --settings=settings_es.cfg --task=create_massmailstatustype_diagram
